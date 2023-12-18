6fbe2ee03cb529eb32687767615fb86381643f52
Anamol Shrestha <anamols100@gmail.com>
2023-12-18 11:51:54 -0500

    style changes

--- course-frontend/src/components/Login.jsx
+++ course-frontend/src/components/Login.jsx
@@ -1,54 +1,57 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
-const Login = ({ setCurrentUser }) => { 
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-      username: '',
-      password: '',
-    });
+const Login = ({ setCurrentUser }) => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-    const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
 
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }; 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
 
-      try {
-        const response = await fetch('http://localhost:5000/api/login', { 
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        });
+      const data = await response.json();
 
-        const data = await response.json();
-        if (!response.ok) {
-          setError(data.message);
-        } else {
-          console.log('Login successful');
-          setCurrentUser(data.user); 
-          navigate('/'); 
-        }
+      if (!response.ok) {
+        setError(data.message);
+      } else {
+        const { token, user } = data;
+        
+        localStorage.setItem('token', token);
 
-      } catch (error) {
-        console.error('Error:', error);
-        setError('Error logging in');
+        setCurrentUser(user);
+
+        navigate('/');
       }
-    };
+    } catch (error) {
+      console.error('Error:', error);
+      setError('Error logging in');
+    }
+  };
 
   return (
     <div className="login-container">
       <h1>Login</h1>
-      <form onSubmit={handleSubmit} className="login-form">
+      <form className="login-form">
         <label htmlFor="username">Username:</label>
         <input
           type="text"
@@ -69,7 +72,9 @@ const Login = ({ setCurrentUser }) => {
           required
         />
 
-        <button type="submit">Login</button>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
 
         {error && <p className="error-message">{error}</p>}
       </form>
