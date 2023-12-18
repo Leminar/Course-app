bbedfd8c43c50960b2d91bac6ad82fb229e8c63c
Anamol Shrestha <anamols100@gmail.com>
2023-12-18 11:12:04 -0500

    final commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 passport.use(new LocalStrategy(async (username, password, done) => {
     const user = await User.findOne({ username: username });
     if (!user || !bcrypt.compareSync(password, user.password)) {
-        return done(null, false, { message: 'Incorrect username or password.' })
+        return done(null, false, { message: 'You have input Incorrect username or password.' })
     }
     return done(null, user);
 }));
